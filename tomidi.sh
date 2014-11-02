920e685eed0ac7e57e1ff38077e01be6b04a29c0
Paolino <paolo.veronelli@com>
2014-11-02 09:42:38 +0100

    arduino files

---
+++ tomidi.sh
@@ -0,0 +1,3 @@
+sudo dfu-programmer at90usb82 erase
+sudo dfu-programmer at90usb82 flash HIDUINO_MIDI.hex
+sudo dfu-programmer at90usb82 reset
---
+++ tousb.sh
@@ -0,0 +1,3 @@
+sudo dfu-programmer at90usb82 erase
+sudo dfu-programmer at90usb82 flash usbserial_uno_16u2.hex
+sudo dfu-programmer at90usb82 reset
